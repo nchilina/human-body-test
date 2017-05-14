897f5c1a9283ac0beee01f1b37480d850100b177
Natalya <natalya.chilina@gmail.com>
2017-05-14 15:06:14 -0400

    initial commit

---
+++ src/app/index.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {HelloComponent} from './hello';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+  ],
+  declarations: [
+    HelloComponent
+  ],
+  bootstrap: [HelloComponent]
+})
+export class AppModule {}
